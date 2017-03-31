bad73307bc3b8b9bdf81a7dd10c9702af56f1a54
dm729 <dm729@cam.ac.uk>
2017-03-31 17:18:06 +0100

    Network_footprint

--- read_AB_data.py
+++ read_AB_data.py
@@ -11,7 +11,8 @@ Table yes or no involvement for each proteins
 Similarity between antibiotics using cosine similarity
 Similarity between antibiotics using Tanimoto Similarity
 """
-
+import pandas as pd
+import igraph
 """
 #This part is the network construction
 import Constructing_interaction_network as constin
@@ -45,10 +46,9 @@ def create_first_neighbours (G, antibioitics_dataset):
     antibioitics_set = set()
     proteins_set = set()
     for antibiotics in antibioitics_dataset.columns:
-        G.vs[antibiotics] = {}
-        G.vs[antibiotics]["types"] = set()
-        G.vs[antibiotics]["sumvalue"] = 0
-        G.vs[antibiotics]["affected"] = 0
+        for node in G.vs:
+            node[antibiotics] = {"types" : set(), "sumvalue" : 0.0, "affected" : 0}
+        #print G.vs[antibiotics]
         antibioitics_set.add(antibiotics)
         AB_proteins = set()
         Neighbour_proteins = set()
@@ -56,24 +56,27 @@ def create_first_neighbours (G, antibioitics_dataset):
             if antibioitics_dataset.at[protein, antibiotics] > 0:
                 if protein in G.vs["name"] and protein not in AB_proteins:
                     node = G.vs.select(name_eq=protein)
+                    node = node[0]
+                    #print node
                     node[antibiotics]["types"].add("Mut")
                     node[antibiotics]["sumvalue"] += 2
                     node[antibiotics]["affected"] = 1
-                    print node["name"]
-                    proteins_set.add(node["name"][0])
-                    AB_proteins.add(node["name"][0])
-                    neighbours = G.neighborhood(node[0])
+                    proteins_set.add(node["name"])
+                    AB_proteins.add(node["name"])
+                    neighbours = G.neighborhood(node)
                     # This part requires a lot to do. It is unknown what information flow method would be the best.
                     # The current method is bit based, if a protein is affected then it is affected whatever way.
                     for neighbour in neighbours:
                         if G.vs[neighbour]["name"] not in Neighbour_proteins:
+                            print G.vs[neighbour]["name"]
                             G.vs[neighbour][antibiotics]["affected"] = 1
                             G.vs[neighbour][antibiotics]["sumvalue"] += 1
-                            G.vs[antibiotics]["types"].add("FN")
+                            G.vs[neighbour][antibiotics]["types"].add("FN")
                             proteins_set.add(G.vs[neighbour]["name"])
                             Neighbour_proteins.add(G.vs[neighbour]["name"])
+
     antibioitics_list = list(antibioitics_set)
-    return G,proteins_set,antibioitics_list
+    return G,proteins_set,antibioitics_list, Neighbour_proteins
 
 
 def read_in_tf_data_from_file_to_igraph_graph(file_, G, proteins_set, antibioitics_list):
@@ -85,39 +88,42 @@ def read_in_tf_data_from_file_to_igraph_graph(file_, G, proteins_set, antibioiti
     """
     inp = open(file_)
     antibiotics_dictionarry_for_TFs = {}
+    inp.readline() #header
     for line in inp:
         line = line.strip()
         line = line.split("\t")
-        if line[0] not in antibiotics_dictionarry_for_TFs and line[0] in antibioitics_list:
-            antibiotics_dictionarry_for_TFs[line[0]] ={}
-            antibiotics_dictionarry_for_TFs[line[0]]["TF"] = set()
-            antibiotics_dictionarry_for_TFs[line[0]]["target"] = set()
-        antibiotics_dictionarry_for_TFs[line[0]]["TF"].add(line[1])
-        antibiotics_dictionarry_for_TFs[line[0]]["target"].add(line[2])
+        if line[2] not in antibiotics_dictionarry_for_TFs and line[2] in antibioitics_list:
+            antibiotics_dictionarry_for_TFs[line[2]] ={}
+            antibiotics_dictionarry_for_TFs[line[2]]["TF"] = set()
+            antibiotics_dictionarry_for_TFs[line[2]]["target"] = set()
+        antibiotics_dictionarry_for_TFs[line[2]]["TF"].add(line[0])
+        antibiotics_dictionarry_for_TFs[line[2]]["target"].add(line[1])
 
     for antibiotic in antibiotics_dictionarry_for_TFs:
-        for TF in antibiotics_dictionarry_for_TFs["TF"]:
+        for TF in antibiotics_dictionarry_for_TFs[antibiotic]["TF"]:
             if TF in G.vs["name"]:
                 node = G.vs.select(name_eq=TF)
+                node = node[0]
                 node[antibiotic]["types"].add("TF")
                 node[antibiotic]["sumvalue"] += 1
                 node[antibiotic]["affected"] = 1
-                proteins_set.add(node["name"][0])
-        for target in antibiotics_dictionarry_for_TFs["target"]:
+                proteins_set.add(node["name"])
+        for target in antibiotics_dictionarry_for_TFs[antibiotic]["target"]:
             if target in G.vs["name"]:
                 node = G.vs.select(name_eq=target)
+                node = node[0]
                 node[antibiotic]["types"].add("target")
                 node[antibiotic]["sumvalue"] += 1
                 node[antibiotic]["affected"] = 1
-                proteins_set.add(node["name"][0])
-                neighbours = G.neighborhood(node[0])
+                proteins_set.add(node["name"])
+                neighbours = G.neighborhood(node)
                 for neighbour in neighbours:
                     if "FN" not in G.vs[neighbour][antibiotic]["types"]:
                         G.vs[neighbour][antibiotic]["affected"] = 1
                         G.vs[neighbour][antibiotic]["sumvalue"] += 1
-                        G.vs[antibiotic]["types"].add("FN")
+                        G.vs[neighbour][antibiotic]["types"].add("FN")
                         proteins_set.add(G.vs[neighbour]["name"])
-    return G
+    return G, proteins_set
 
 
 def write_out_graph_dataset(G, proteins_set, antibioitics_list, out_file):
@@ -128,7 +134,14 @@ def write_out_graph_dataset(G, proteins_set, antibioitics_list, out_file):
     for protein in proteins_list:
         out.write(protein)
         node = G.vs.select(name_eq=protein)
+        node = node[0]
         for antibiotics in antibioitics_list:
-            out.write("\t"+str(node[antibiotics]["sumvalue"]))
+            out.write("\t"+str(",".join(node[antibiotics]["types"])))
         out.write("\n")
     out.close()
+
+antibioitics_dataset = pd.read_csv("31_03_2017_AB_mutation.txt", index_col=0, sep="\t", header=0)
+G = igraph.Graph.Read_Ncol("07_11_2016_DIP_RAJAGOPOLA_MPIDB_E_coli_K12_PPI.ncol")
+G,proteins_set,antibioitics_list, Neighbour_proteins = create_first_neighbours(G, antibioitics_dataset)
+G, proteins_set = read_in_tf_data_from_file_to_igraph_graph("31_03__2017_TF_targets_datasset.txt",G,proteins_set,antibioitics_list)
+write_out_graph_dataset (G,proteins_set,antibioitics_list, "31_03_2017_affected_types.txt")
